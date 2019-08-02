4d239cf64c1951e6fc3b9497f61c373c2d8776b1
RakeshPardeshi <RakeshPardeshi@users.noreply.github.com>
2019-08-02 20:52:28 +0530

    Update 1_DT_INTRODUCTION.R

--- 1_DT_INTRODUCTION.R
+++ 1_DT_INTRODUCTION.R
@@ -15,53 +15,45 @@
 ### 2. Intialize the workspace by loading required libraries
   library(rpart)
   library(rattle)
+  library(caret)
 
 ### 3. Import the dataset
-  mydata= read.csv(file = ".\\Data\\HR_Attrition_Dataset.csv")
+  hr_data= read.csv(file = ".\\Data\\HR_Attrition_Dataset.csv")
   sapply(mydata, class)
 
 ### 4. Preprocessing Data
   ## 4.1 Make dependent variable as a factor (categorical)
-  mydata$left = as.factor(mydata$left)
+  hr_data$left = as.factor(hr_data$left)
  
 
 ### 5. Divide the data into 80% Training and 20% Testing datasets
-
+  set.seed(3456)
+  trainIndex <- createDataPartition(hr_data$left, p = 0.8,  list = FALSE, times = 1)
+  hr_train <- hr_data[trainIndex, ]
+  hr_test <- hr_data[-trainIndex, ]
+  
 
 ### 6. Construct Decision Tree Model
-  ## 6.1 Define the formula
+  mtree <- rpart(formula=left ~., data = hr_train, method="class")
+  mtree
+  fancyRpartPlot(mtree)
+  
 
 
-mtree <- rpart(formula=left ~., data = mydata, method="class")
-mtree
+### 7.   Evaluate the performance of the model
+  ##7.1 Performance on training data
+  train_prediction_label = predict(mtree, newdata = hr_test, type = "class")
+  train_prediction_label
+  train_result = cbind.data.frame(hr_train, prediction_label = train_prediction_label)
+  
 
-#control = rpart.control(minsplit = 1, minbucket = 1, maxdepth = 10, usesurrogate = 2, xval =10 )
-fancyRpartPlot(mtree)
-
-predict_label = predict(mtree, type = "class")
-#predict_label
-
-tree_result = cbind.data.frame(mydata, predict_label = predict_label)
-
-
-### 7   Evaluate the performance of the model
-#CARET package functions
-#install.packages("e1071")
-library(caret)
-confusionMatrix(tree_result$predict_label, tree_result$left, positive = "1")
+  ##7.2 Performance on testing data
+  test_prediction_label = predict(mtree,  newdata = hr_test, type = "class")
+  test_prediction_label
+  test_result = cbind.data.frame(hr_test, prediction_label = test_prediction_label)
 
 
-### 8   Finding Optimal Decision tree model using pre-pruning
+### 8. Evaluate the performance of the model
+train_cf  = confusionMatrix(train_result$prediction_label, train_result$left, positive = "1")
+test_cf = confusionMatrix(test_result$prediction_label, test_result$left, positive = "1")
 
-
-
-### 9   Finding Optimal Decision tree model using post-pruning
-
-
-
-### 10  Model Selection based on performance metrics
-
-
-
-### 11. Extracting Rules
-asRules(mtree)
